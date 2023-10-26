a2c44246d232266c0fda61e698e05ae17c8a0818
Brusmel Gil <Brusmel@outlook.es>
2023-10-26 10:06:19 +0200

    pizza

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-let ingredientes = [
-"masa precocida",
-"salsa de tomate",
-"200 grs de queso",
-"bacon",
-"carne picada"
-] 
+/* import promptSync from "prompt-sync"; */
 
+import { pregunta } from "./funciones.js";
+ 
 function app(){
-    makepizza();
+    pregunta();
 
 }
-app();
+app(); 
 
-function makepizza(){
-    prepararMasa();
-    añadirIngredientes();
 
-    console.log("meter al horno 15 minutos");
-    console.log("sacar la pizza ya preparada y lista para comer");
-}
-
-function prepararMasa(){
-    console.log("Sacar la masa de su paquete");
-    console.log("Estirar la masa en una bandeja");
-}
-
-function añadirIngredientes(){
-    for(let i=0; i < ingredientes.length; i++){
-        console.log("Añadir", ingredientes[i])
-    }
-}
 
 
